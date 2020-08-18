f4febb5d93e7bf67811dffef072c5b76f1a354c6
vincentkwang1 <45569552+vincentkwang1@users.noreply.github.com>
2020-08-17 20:55:39 -0700

    commented

--- sprites.py
+++ sprites.py
@@ -15,28 +15,26 @@ class bg(pg.sprite.Sprite):#drawn under the pieces. just a white line for now
 
 class piece(pg.sprite.Sprite):
     def __init__(self, game,x,y,type):
-        self.sides = []
+        self.sides = [] #list of 4 that describes what the sides are. 1-4 for operations, None for none, False for in side, 5 for the start piece
         self.holding = False
         self.shadow_height = SHADOWHEIGHT
         self.game = game
         self.quarter_pos = [[IMGSIZE/2,(IMGSIZE-TILESIZE)/2],[(IMGSIZE-TILESIZE)/2,0],[0,(IMGSIZE-TILESIZE)/2],[(IMGSIZE-TILESIZE)/2,IMGSIZE/2]]
         self.text_quarter_pos = [[IMGSIZE-OFFSET,IMGSIZE/2],[IMGSIZE/2,OFFSET],[OFFSET,IMGSIZE/2],[IMGSIZE/2,IMGSIZE-OFFSET]]
         self.in_quarter_pos = [[IMGSIZE-OFFSET2,IMGSIZE/2],[IMGSIZE/2,OFFSET2],[OFFSET2,IMGSIZE/2],[IMGSIZE/2,IMGSIZE-OFFSET2]]
-        self.type = type
+        self.type = type #0 = normal, 1 = start, 2 = end
         self.connected_to = [] #OUTGOING connections. 4*5, 4's connected to is 5
         self.connected_from = None #INGOING connections. only necessary for final piece
-        self.replaced_op = None #the operation type that is replaced by =
+        self.replaced_op = None #the operation type that is replaced by = when connected to end piece (so it is saved when removed from end piece)
         if self.type == 0:
             self.num = 1+r.randrange(9)
         elif self.type == 1:
             self.num = 'start'
         else:
             self.num = 10 + r.randrange(40)
-        self.out_coords = []
-        self.in_coords = []
         self.on_board = False
-
-        if type == 0: #normal piece
+        'makes sides of the pieces based on type'
+        if type == 0: #normal piece. rerolls sides until they're not all in or all out.
             found = False
             while not found:
                 self.sides = [1+r.randrange(4)]
@@ -61,6 +59,7 @@ class piece(pg.sprite.Sprite):
         return False
 
     def make_image(self):
+        'CALL TO REFRESH IMAGE. its a mess, ill decipher it later'
         self.image = pg.Surface((IMGSIZE+self.shadow_height,IMGSIZE+self.shadow_height))
         self.image.set_colorkey(BLACK)
         if self.type == 0:
@@ -123,6 +122,7 @@ class piece(pg.sprite.Sprite):
             self.on_board = False
 
     def rotate(self,dir):
+        #rotates by changign index of sides
         if dir == 'q':
             self.sides = self.sides[-1:]+self.sides[:-1]
         if dir == 'e':
@@ -130,6 +130,7 @@ class piece(pg.sprite.Sprite):
         self.make_image()
 
     def draw_shadow(self):
+        #black magic. not sure how it works
         self.mask.invert()
         to_surface = self.mask.to_surface()
         to_surface.set_colorkey((255,255,255))
@@ -138,7 +139,6 @@ class piece(pg.sprite.Sprite):
         to_surface.blit(self.image,[0,0])
         self.image = to_surface.copy()
         self.image.set_colorkey(BLACK)
-        #to_surface.set_colorkey(BLACK)
 
     def draw_text(self,surface,text,x,y,color,size):
         my_file = os.path.join(self.game.game_folder, 'calibri.ttf')
@@ -150,12 +150,13 @@ class piece(pg.sprite.Sprite):
 
     def check_click(self,mouse_pos,click_dir):
         if self.rect.collidepoint(mouse_pos):
-            if self.type == 2 and self.connected_from != None:#if end piece
+            if self.type == 2 and self.connected_from != None:#if end piece, put replaced operation back
                 for i in range(4):
                     if self.connected_from.sides[i] == 6:
                         self.connected_from.sides[i] = self.connected_from.replaced_op
+            #mask collision
             pos_in_mask = mouse_pos[0] - self.rect.x, mouse_pos[1] - self.rect.y
-            if self.mask.get_at(pos_in_mask) == 1:
+            if self.mask.get_at(pos_in_mask) == 1:#if picked up, remove all connections
                 self.holding = True
                 self.connected_to = []
                 self.connected_from = None
@@ -176,14 +177,15 @@ class piece(pg.sprite.Sprite):
         self.image = merged
 
     def find_connection(self,pieces):
-        min_dist = MIN_DIST
+        #when piece is put down, looks around for pieces to connect to
+        min_dist = MIN_DIST #min distance for snapping
         other_piece = None
         other_side = None
         in_side = None
-        for i in range(4):
+        for i in range(4): #checks each side
             if self.sides[i] == False:#in side
                 in_point = [self.in_quarter_pos[i][0]+self.rect.x,self.in_quarter_pos[i][1]+self.rect.y]
-                for piece in pieces:
+                for piece in pieces:#looks through all pieces for a piece that is rotated the right way, the opposite operation, and close enough
                     if piece.rect.center != self.rect.center and piece.on_board:
                         for j in range(4):
                             if piece.sides[j] != None and piece.sides[j] != False:#if side is an operation
@@ -208,13 +210,13 @@ class piece(pg.sprite.Sprite):
                                             other_piece = piece
                                             other_side = j
                                             in_side = i
-        if other_piece != None:
+        if other_piece != None:#if found another piece, snap it based on the side
             self.snap(other_piece,in_side)
             return [other_piece.type,other_side,in_side]
         else:
             return None
 
-    def snap(self,target,own_side):
+    def snap(self,target,own_side):#snaps based on which side the connection is
         anchor_point = target.rect.center
         if own_side == 0:
             self.rect.center = [anchor_point[0]-TILESIZE,anchor_point[1]]
@@ -228,13 +230,13 @@ class piece(pg.sprite.Sprite):
         else:
             self.rect.center = [anchor_point[0],anchor_point[1]-TILESIZE]
             target_side = 1
-        if not self.exists(target):
+        if not self.exists(target): #only adds to connected_to if it doesnt exist already
             target.connected_to.append([self,target.sides[target_side]])
         if self.type == 2:#if connecting to end node
             target.replaced_op = target.sides[target_side]
             target.sides[target_side] = 6
             self.connected_from = target
-        self.rectx = self.rect.x
+        self.rectx = self.rect.x#set new positions
         self.recty = self.rect.y
 
     def exists(self,target):
@@ -262,26 +264,14 @@ class piece(pg.sprite.Sprite):
         self.make_image()
 
     def update(self):
-        self.out_coords = []
-        self.in_coords = []
-        for connection in self.connected_to:
+        for connection in self.connected_to:#if one of connected pieces is pieced up, remove it from connected_to
             if connection[0].holding:
                 self.connected_to.pop(self.connected_to.index(connection))
         if self.rect.x < WIDTH-TILESIZE*2.5:
             self.on_board = True
         else:
             self.on_board = False
-        if self.type == 0 and self.on_board:
-            for i in range(4):
-                if self.sides[i] != False:
-                    self.out_coords.append([self.text_quarter_pos[i][0]+self.rectx,self.text_quarter_pos[i][1]+self.recty])
-                else:
-                    self.in_coords.append([self.in_quarter_pos[i][0]+self.rectx,self.in_quarter_pos[i][1]+self.recty])
-        elif self.type == 1:
-            self.out_coords.append([self.text_quarter_pos[0][0]+self.rectx,self.text_quarter_pos[0][1]+self.recty])
-        elif self.type == 2:
-            self.in_coords.append([self.in_quarter_pos[2][0]+self.rectx,self.in_quarter_pos[2][1]+self.recty])
         if self.holding:
-            self.rect.center = self.game.mouse_pos
+            self.rect.center = self.game.mouse_pos#if being held, keep the image on the mouse
             self.rectx = self.rect.x
             self.recty = self.rect.y
